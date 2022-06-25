c82216e94e1723b14bda55fdb4bfee1887f3e93b
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2022-06-25 15:11:44 +0200

    articles: Add right margin to dates in index

--- articles/pages/makeindex.sh
+++ articles/pages/makeindex.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-echo '<ul>'
 for art in $(find . -name "*.md"); do
     cfg="$(basename $art .md).cfg"
     cat <<EOF
-<span style="float:left;"><a href="$(basename $art .md).html">$(grep title $cfg | cut -d\= -f2)</a></span><span style="float:right;">$(stat -c "%y" $art | cut -d\  -f1)</span><br>
+<span style="float:left;"><a href="$(basename $art .md).html">$(grep title $cfg | cut -d\= -f2)</a></span><span style="float:right; margin-right: 5px;">$(stat -c "%y" $art | cut -d\  -f1)</span><br>
 EOF
 done
-
-echo '</ul>'
