562736dbfb1e95124b4c4b5d41ae4ca4a29e2534
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2022-07-03 22:33:08 +0200

    Get date from cfg files

--- pages/makeindex.sh
+++ pages/makeindex.sh
@@ -3,6 +3,6 @@ for art in $(find ../articles/pages/ -name "*.md"); do
     cfg="$(basename "$art" .md).cfg"
     html="$(basename "$art" .md).html"
     cat <<EOF
-<span style="float:left;"><a href="/articles/$html">$(grep title ../articles/pages/"$cfg" | cut -d\= -f2)</a></span><span style="float:right;margin-right: 5px;">$(stat -c "%y" "$art" | cut -d\  -f1)</span><br>
+<span style="float:left;"><a href="/articles/$html">$(grep title ../articles/pages/"$cfg" | cut -d\= -f2)</a></span><span style="float:right;margin-right: 5px;">$(grep '^date' ../articles/pages/"$cfg" | cut -d\= -f2)</span><br>
 EOF
 done
